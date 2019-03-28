246b88f4486624042ae3a10a70ec585e1ee86352
wiemrh <ouiemm.je@gmail.com>
2019-03-28 14:02:24 +0100

    list des offres add , modif back and front

--- app/src/main/java/com/wiem/sospets/Fragment_Offres.java
+++ app/src/main/java/com/wiem/sospets/Fragment_Offres.java
@@ -1,47 +1,202 @@
 package com.wiem.sospets;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Fragment_Offres extends Fragment {
 
-    FloatingActionButton add ;
-    RecyclerView recyclerView;
-    RecyclerView.LayoutManager layoutManager;
-    RecyclerView.Adapter adapter;
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private Adapter adapter;
+    private List<Pets> petsList;
+    ApiInterface apiInterface;
+    Adapter.RecyclerViewClickListener listener;
+    ProgressBar progressBar;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View result =  inflater.inflate(R.layout.fragment_offres, container, false);
-        add = result.findViewById(R.id.fab);
-        add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                 getFragmentManager().beginTransaction().replace(R.id.fragment_container , new Fragment_Add_Offer()).commit(); }});
+        final View result =  inflater.inflate(R.layout.fragment_offres, container, false);
+        setHasOptionsMenu(true);
+        apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
 
-
-        recyclerView = result.findViewById(R.id.recycler_view);
+        progressBar = result.findViewById(R.id.progress);
+        recyclerView = result.findViewById(R.id.recyclerView);
 
         layoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(layoutManager);
 
-        adapter = new RecyclerAdapter();
-        recyclerView.setAdapter(adapter);
+        listener = new Adapter.RecyclerViewClickListener() {
+            @Override
+            public void onRowClick(View view, final int position) {
+
+                Intent intent = new Intent(getContext(), EditorActivity.class);
+                intent.putExtra("id", petsList.get(position).getId());
+                intent.putExtra("name", petsList.get(position).getName());
+                intent.putExtra("species", petsList.get(position).getSpecies());
+                intent.putExtra("breed", petsList.get(position).getBreed());
+                intent.putExtra("gender", petsList.get(position).getGender());
+                intent.putExtra("picture", petsList.get(position).getPicture());
+                intent.putExtra("birth", petsList.get(position).getBirth());
+                startActivity(intent);
+
+            }
+
+            @Override
+            public void onLoveClick(View view, int position) {
+
+                final int id = petsList.get(position).getId();
+                final Boolean love = petsList.get(position).getLove();
+                final ImageView mLove = view.findViewById(R.id.love);
+
+                if (love){
+                    mLove.setImageResource(R.drawable.likeof);
+                    petsList.get(position).setLove(false);
+                    updateLove("update_love", id, false);
+                    adapter.notifyDataSetChanged();
+                } else {
+                    mLove.setImageResource(R.drawable.likeon);
+                    petsList.get(position).setLove(true);
+                    updateLove("update_love", id, true);
+                    adapter.notifyDataSetChanged();
+                }
+
+            }
+        };
+        FloatingActionButton fab = result.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getContext(), EditorActivity.class));
+            }
+        });  return result ; }
 
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
 
-              return result ; }
+        inflater.inflate(R.menu.menu_main, menu) ;
+
+        SearchManager searchManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
+        final SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        MenuItem searchMenuItem = menu.findItem(R.id.action_search);
+
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getActivity().getComponentName())
+        );
+        searchView.setQueryHint("Search Pet...");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(final String query) {
+
+                adapter.getFilter().filter(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+        searchMenuItem.getIcon().setVisible(false, false);
 
 
-}
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void getPets(){
+
+        Call<List<Pets>> call = apiInterface.getPets();
+        call.enqueue(new Callback<List<Pets>>() {
+            @Override
+            public void onResponse(Call<List<Pets>> call, Response<List<Pets>> response) {
+                progressBar.setVisibility(View.GONE);
+                petsList = response.body();
+                Log.i(MainActivity.class.getSimpleName(), response.body().toString());
+                adapter = new Adapter(petsList, getContext(), listener);
+                recyclerView.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<List<Pets>> call, Throwable t) {
+                Toast.makeText(getContext(), "rp :"+
+                                t.getMessage().toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void updateLove(final String key, final int id, final Boolean love){
+
+        Call<Pets> call = apiInterface.updateLove(key, id, love);
+
+        call.enqueue(new Callback<Pets>() {
+            @Override
+            public void onResponse(Call<Pets> call, Response<Pets> response) {
+
+                Log.i(MainActivity.class.getSimpleName(), "Response "+response.toString());
+
+                String value = response.body().getValue();
+                String message = response.body().getMassage();
+
+                if (value.equals("1")){
+                    Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<Pets> call, Throwable t) {
+                Toast.makeText(getContext(), t.getMessage().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+
+            super.onResume();
+            getPets();
+        }
+    }
+
